e73bcfbe7b8a4eccfcf1582450b6ba951f93a62f
Vishwa Patel <102425875+vishwa-hp@users.noreply.github.com>
2023-11-30 16:52:47 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import TodoList from "./Components/TodoList";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
